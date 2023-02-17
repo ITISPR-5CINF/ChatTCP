cba297734055484a8ad8171c96b314cf8dccfb49
Alessandro <alessandroromano2005@gmail.com>
2023-02-17 15:43:10 +0100

    ChatTCP: Client: Implementa form di registrazione

---
+++ Client/FormSignin.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ChatTCP.Client
+{
+    partial class FormSignin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSignin = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCognome = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.NomeTextBox = new System.Windows.Forms.TextBox();
+            this.CognomeTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.SigninButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSignin
+            // 
+            this.lblSignin.AutoSize = true;
+            this.lblSignin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSignin.Location = new System.Drawing.Point(12, 9);
+            this.lblSignin.Name = "lblSignin";
+            this.lblSignin.Size = new System.Drawing.Size(82, 25);
+            this.lblSignin.TabIndex = 0;
+            this.lblSignin.Text = "Sign-In";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(14, 62);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblCognome
+            // 
+            this.lblCognome.AutoSize = true;
+            this.lblCognome.Location = new System.Drawing.Point(14, 97);
+            this.lblCognome.Name = "lblCognome";
+            this.lblCognome.Size = new System.Drawing.Size(52, 13);
+            this.lblCognome.TabIndex = 2;
+            this.lblCognome.Text = "Cognome";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(14, 132);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(14, 166);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // NomeTextBox
+            // 
+            this.NomeTextBox.Location = new System.Drawing.Point(82, 59);
+            this.NomeTextBox.Name = "NomeTextBox";
+            this.NomeTextBox.Size = new System.Drawing.Size(209, 20);
+            this.NomeTextBox.TabIndex = 5;
+            // 
+            // CognomeTextBox
+            // 
+            this.CognomeTextBox.Location = new System.Drawing.Point(82, 94);
+            this.CognomeTextBox.Name = "CognomeTextBox";
+            this.CognomeTextBox.Size = new System.Drawing.Size(209, 20);
+            this.CognomeTextBox.TabIndex = 6;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(82, 129);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(209, 20);
+            this.UsernameTextBox.TabIndex = 7;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(82, 163);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(209, 20);
+            this.PasswordTextBox.TabIndex = 8;
+            // 
+            // SigninButton
+            // 
+            this.SigninButton.Location = new System.Drawing.Point(17, 218);
+            this.SigninButton.Name = "SigninButton";
+            this.SigninButton.Size = new System.Drawing.Size(52, 29);
+            this.SigninButton.TabIndex = 9;
+            this.SigninButton.Text = "Sign-In";
+            this.SigninButton.UseVisualStyleBackColor = true;
+            // 
+            // FormSignin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 254);
+            this.Controls.Add(this.SigninButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.CognomeTextBox);
+            this.Controls.Add(this.NomeTextBox);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lblCognome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblSignin);
+            this.Name = "FormSignin";
+            this.Text = "Sign-In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSignin;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCognome;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox NomeTextBox;
+        private System.Windows.Forms.TextBox CognomeTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button SigninButton;
+    }
+}
